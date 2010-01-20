e9c81c1ea271feced9a163aa4c79e88d00c47d60
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-01-20 02:02:51 +0000

    Moving processing stuff to the Model

--- lib/Avarpics/Controller/Day.pm
+++ lib/Avarpics/Controller/Day.pm
@@ -1,9 +1,6 @@
 package Avarpics::Controller::Day;
 use Moose;
 use namespace::autoclean;
-use Date::Calc qw(Add_Delta_Days);
-use POSIX 'strftime';
-use WWW::Mechanize;
 
 BEGIN {extends 'Catalyst::Controller'; }
 
@@ -32,7 +29,7 @@ sub index :Path :CaptureArgs(1) {
     $day = $log->latest_day() unless $day;
 
     if ( $log->valid_day( $day ) ) {
-        my %vars = $self->_process( $c, $day );
+        my %vars = $log->data_for_day( $day );
         while (my ($k, $v) = each %vars) {
             $c->stash->{$k} = $v;
         }
@@ -40,128 +37,9 @@ sub index :Path :CaptureArgs(1) {
         $c->error( "$day is not a valid day" );
     }
 
-    # Don't show next links if it's today
-    my $today = strftime("%Y-%m-%d", localtime);
-    if ($today eq $day) {
-        $c->stash->{no_next} = 1;
-    }
-
+    $c->stash->{no_next} = 1 if $log->is_today($day);
     $c->stash->{title} = sprintf "Pics from %s for %s", $c->config->{channel}, $day;
     $c->stash->{template} = 'day.tt';
-}
-
-sub _process {
-	my ($self, $c, $date) = @_;
-
-    my $log = $c->model("Log");
-
-    my $text = $log->on_date_slurp( $date );
-
-	#error('nonesuch') unless -f $file;
-	#error('problemo') unless -r $file;
-
-	my ($current_nick, %seen, @uris);
-
-	# Need to define this outside loop as it has to persist from
-	# a *chan URI (which is not kept) to the following ImageShack URI.
-	my $comment;
-
-	my $img_count = 0;
-	my $vid_count = 0;
-	
-	foreach my $line (split /\n/, $text) {
-		if ($line =~ /<(?:\@|\+| )(\w+)>/) {
-			$current_nick = $1;
-		}
-				
-		my $fileext = qr/\.(?:jpg|jpeg|gif|png|bmp)/i;
-		
-		if ($line =~ m{(http://[\S]+?$fileext)}) {
-			my $uri = $1;
-			
-			if ($line =~ /\s+#\s+(.*)/) {
-				$comment = $1;
-			}
-
-			if (!$seen{$uri} && $uri !~ /\dchan\./) {
-
-				push @uris, {
-					'type'    => 'img',
-					'uri'     => $uri,
-					'who'     => $current_nick,
-					'comment' => $comment,
-				};
-
-				$img_count++;		
-				undef $comment;	
-			}
-
-			$seen{$uri} = 1;
-		} elsif ($line =~ m#youtube\.com/watch\?v=(\w{11})#) {
-			my $id = $1;
-
-			my ($vid_comment) = $line =~ /\s+#\s+(.*)/g;
-
-			push @uris, {
-				'type'    => 'vid',
-				'id'      => $id,
-				'who'     => $current_nick,
-				'comment' => $vid_comment,
-			};
-			
-			$vid_count++;
-		} elsif ($line =~ m#(http://www.flickr.com/photos/\w+/\d{10}/)#) {
-			my $flickr = $1;
-			my ($flickr_id) = $flickr =~ m#(\d{10})#;
-			
-			my $mech = WWW::Mechanize->new;
-			$mech->agent_alias('Windows IE 6');
-			$mech->get($flickr);
-			next unless $mech->success;
-			
-			$mech->content =~ m#<link rel="image_src" href="(http://farm\d.static.flickr.com/\d+/\d{10}_\w{10}_\w.jpg)" />#gs;
-			my $uri = $1;
-			next unless $uri;
-			
-			if (!$seen{$uri}) {
-
-				push @uris, {
-					'type'      => 'img',
-					'uri'       => $uri,
-					'who'       => $current_nick,
-					'comment'   => $comment,
-					'flickr_id' => $flickr_id,
-				};
-
-				$img_count++;		
-				undef $comment;	
-			}
-
-			$seen{$uri} = 1;
-		}
-	}
-
-	my $options = {
-		'date'      => $date,
-		'uris'      => \@uris,
-		'img_count' => $img_count,
-		'vid_count' => $vid_count,
-	};
-
-	my ($year, $month, $day) = $date =~ /(\d\d\d\d)-(\d\d)-(\d\d)/g;
-
-	my ($pdy, $pdm, $pdd) = Add_Delta_Days($year, $month, $day, -1);
-	$pdm = sprintf("%02d", $pdm);
-	$pdd = sprintf("%02d", $pdd);
-
-	my ($ndy, $ndm, $ndd) = Add_Delta_Days($year, $month, $day, 1);
-	$ndm = sprintf("%02d", $ndm);
-	$ndd = sprintf("%02d", $ndd);
-
-	$options->{'prev_day'} = join '-', ($pdy, $pdm, $pdd);
-	$options->{'next_day'} = join '-', ($ndy, $ndm, $ndd); 
-
-    %$options;
 }
 
 =head1 AUTHOR
--- lib/Avarpics/Log.pm
+++ lib/Avarpics/Log.pm
@@ -1,6 +1,9 @@
 package Avarpics::Log;
 use Moose;
 use File::Slurp 'slurp';
+use POSIX 'strftime';
+use Date::Calc qw(Add_Delta_Days);
+use WWW::Mechanize;
 
 has logdir => ( is => 'ro', required => 1 );
 has logpre => ( is => 'ro', required => 1 );
@@ -43,6 +46,16 @@ sub latest_day
     ($self->files())[0];
 }
 
+sub is_today
+{
+    my ($self, $day) = @_;
+
+    # Don't show next links if it's today
+    my $today = strftime("%Y-%m-%d", localtime);
+
+    return $today eq $day;
+}
+
 sub on_date_slurp
 {
     my ($self, $date) = @_;
@@ -54,6 +67,115 @@ sub on_date_slurp
 	my $file = "$LOGDIR/$LOGPRE$date.$LOGEXT";
 
     slurp($file);
+}
+
+sub data_for_day {
+    my ($self, $date) = @_;
+
+    my $text = $self->on_date_slurp( $date );
+
+    my ($current_nick, %seen, @uris);
+
+    # Need to define this outside loop as it has to persist from
+    # a *chan URI (which is not kept) to the following ImageShack URI.
+    my $comment;
+
+    my $img_count = 0;
+    my $vid_count = 0;
+
+    foreach my $line (split /\n/, $text) {
+        if ($line =~ /<(?:\@|\+| )(\w+)>/) {
+            $current_nick = $1;
+        }
+
+        my $fileext = qr/\.(?:jpg|jpeg|gif|png|bmp)/i;
+
+        if ($line =~ m{(http://[\S]+?$fileext)}) {
+            my $uri = $1;
+
+            if ($line =~ /\s+#\s+(.*)/) {
+                $comment = $1;
+            }
+
+            if (!$seen{$uri} && $uri !~ /\dchan\./) {
+
+                push @uris, {
+                    'type'    => 'img',
+                    'uri'     => $uri,
+                    'who'     => $current_nick,
+                    'comment' => $comment,
+                };
+
+                $img_count++;       
+                undef $comment; 
+            }
+
+            $seen{$uri} = 1;
+        } elsif ($line =~ m#youtube\.com/watch\?v=(\w{11})#) {
+            my $id = $1;
+
+            my ($vid_comment) = $line =~ /\s+#\s+(.*)/g;
+
+            push @uris, {
+                'type'    => 'vid',
+                'id'      => $id,
+                'who'     => $current_nick,
+                'comment' => $vid_comment,
+            };
+
+            $vid_count++;
+        } elsif ($line =~ m#(http://www.flickr.com/photos/\w+/\d{10}/)#) {
+            my $flickr = $1;
+            my ($flickr_id) = $flickr =~ m#(\d{10})#;
+
+            my $mech = WWW::Mechanize->new;
+            $mech->agent_alias('Windows IE 6');
+            $mech->get($flickr);
+            next unless $mech->success;
+
+            $mech->content =~ m#<link rel="image_src" href="(http://farm\d.static.flickr.com/\d+/\d{10}_\w{10}_\w.jpg)" />#gs;
+            my $uri = $1;
+            next unless $uri;
+
+            if (!$seen{$uri}) {
+
+                push @uris, {
+                    'type'      => 'img',
+                    'uri'       => $uri,
+                    'who'       => $current_nick,
+                    'comment'   => $comment,
+                    'flickr_id' => $flickr_id,
+                };
+
+                $img_count++;       
+                undef $comment; 
+            }
+
+            $seen{$uri} = 1;
+        }
+    }
+
+    my $options = {
+        'date'      => $date,
+        'uris'      => \@uris,
+        'img_count' => $img_count,
+        'vid_count' => $vid_count,
+    };
+
+    my ($year, $month, $day) = $date =~ /(\d\d\d\d)-(\d\d)-(\d\d)/g;
+
+    my ($pdy, $pdm, $pdd) = Add_Delta_Days($year, $month, $day, -1);
+    $pdm = sprintf("%02d", $pdm);
+    $pdd = sprintf("%02d", $pdd);
+
+    my ($ndy, $ndm, $ndd) = Add_Delta_Days($year, $month, $day, 1);
+    $ndm = sprintf("%02d", $ndm);
+    $ndd = sprintf("%02d", $ndd);
+
+    $options->{'prev_day'} = join '-', ($pdy, $pdm, $pdd);
+    $options->{'next_day'} = join '-', ($ndy, $ndm, $ndd); 
+
+    %$options;
 }
 
 __PACKAGE__->meta->make_immutable;
